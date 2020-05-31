dfbf4155d13a6716ad437c27dc57ffc2cc5245c6
UcanRossette <66195279+UcanRossette@users.noreply.github.com>
2020-05-31 11:47:57 -0500

    Add files via upload

---
+++ 3er parcial/js/rompe_cabezas.js
@@ -0,0 +1,79 @@
+/*Programacion de JavaScript*/
+var piezas = document.getElementsByClassName('movil');
+
+var tamWidh = [288,298,345,350,200,340,230,200,240];
+var tamheight=[300,229,244,235,228,230,200,170,280];
+for(var i=0 ; i<piezas.length;i++){
+	piezas[i].setAttribute("width", tamWidh[i]);
+	piezas[i].setAttribute("height", tamheight[i]);
+	piezas[i].setAttribute("x",Math.floor((Math.random()*10)+1));
+	piezas[i].setAttribute("y",Math.floor((Math.random()*409)+1));
+	piezas[i].setAttribute("onmousedown","seleccionarElemento(evt)");
+}
+
+var elementSelect=0;
+var currentX=0;
+var currentY=0;
+var currentPosX=0;
+var currentPosY=0;
+
+function seleccionarElemento(evt) {
+	elementSelect=reordenar(evt);
+	currentX=evt.clientX;
+	currentY=evt.clientY;
+	currentPosx=parseFloat(elementSelect.getAttribute("x"));
+	currentPosy=parseFloat(elementSelect.getAttribute("y"));
+	elementSelect.setAttribute("onmousemove","moverElemento(evt)");
+
+}
+function moverElemento(evt){
+	var dx=evt.clientX - currentX;
+	var dy=evt.clientY - currentY;
+	currentPosx=currentPosx + dx;
+	currentPosy=currentPosy + dy;
+	elementSelect.setAttribute("x",currentPosx);
+	elementSelect.setAttribute("y",currentPosy);
+	currentX=evt.clientX;
+	currentY=evt.clientY;
+	elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
+	elementSelect.setAttribute("onmouseup","deseleccionarElemento(evt)");
+		iman();
+}
+function deseleccionarElemento(evt){
+	if (elementSelect !=0) {
+		elementSelect.removeAttribute("onmousemove");
+		elementSelect.removeAttribute("onmouseout");
+		elementSelect.removeAttribute("onmouseup");
+		elementSelect=0;
+	}
+}
+function reordenar(evt){
+	var padre = evt.target.parentNode;
+	var clone =padre.cloneNode(true);
+	var id=padre.getAttribute("id");
+	entorno.removeChild(document.getElementById(id));
+    entorno.appendChild(clone);
+    return entorno.lastChild.firstChild;
+}
+var origX=[172,353,520,132,393,515,195,394,564];
+var origY=[77,99,101,318,320,318,520,535,485];
+function iman(){
+	for(var i=0;i<piezas.length;i++){
+		if (Math.abs(currentPosx-origX[i])<3 && Math.abs(currentPosy-origY[i]<3)) {
+			elementSelect.setAttribute("x",origX[i]);
+			elementSelect.setAttribute("y",origY[i]);
+		}
+	}
+}
+function testing(){
+	var bien_ubicada =0;
+	var padres = document.getElementsByClassName('padre');
+	for(var i=0;i<piezas.length;i++){
+		var posx =parseFloat(padres[i].firstChild.getAttribute("x"));
+		var posy =parseFloat(padres[i].firstChild.getAttribute("y"));
+		ide =padres[i].getAttribute("id");
+		if (origX[ide]==posx &&origY[ide]==posy) {
+			bien_ubicada=bien_ubicada+1;
+		}
+	}
+}
